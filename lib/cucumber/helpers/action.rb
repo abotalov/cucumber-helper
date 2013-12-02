b24f040edfe47ef5df3a28b0afee7ad3a76c9213
Andrey Botalov <botalov.andrey@gmail.com>
2013-12-02 13:59:12 +0300

    Add checking for mixed content at HTTPS pages

--- lib/cucumber/helpers/action.rb
+++ lib/cucumber/helpers/action.rb
@@ -21,9 +21,32 @@ module Action
   end
 
   private
+
   def assert_html
-    Capybara.html.should_not include Cucumber::Helper.error_message if Cucumber::Helper.error_message
+    expect(Capybara.html).not_to include(Cucumber::Helper.error_message) if Cucumber::Helper.error_message
+
+    if Capybara.current_url.start_with? 'https'
+      incorrect_urls = []
+      [
+        ['img', 'src']
+        ['script', 'src']
+        ['link', 'href'],
+        ['form', 'action'],
+        ['video', 'data-stream'],
+        ['source', 'src'],
+        ['audio', 'src'],
+        ['iframe', 'src'],
+        ['object', 'src'],
+      ].each do |tag_attr|
+        Capybara.all(tag_attr[0], visible: false).each do |tag|
+          attribute = tag[tag_attr[1]]
+          if attribute
+            incorrect_urls << attribute if attribute.downcase.start_with?('http://')
+          end
+        end
+      end
+      expect(incorrect_urls).to be_empty
+    end
   end
 end
 World(Action)
-
